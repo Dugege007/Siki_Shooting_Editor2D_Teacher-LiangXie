3dd0f269eae9474555cd49c570fa0f132651033a
Dugege007 <351080175@qq.com>
2023-08-23 22:21:37 +0800

    011-关卡存储格式制定 和 解析运行

--- Assets/Scripts/ViewController/LevelEditor/LevelPlayer.cs
+++ Assets/Scripts/ViewController/LevelEditor/LevelPlayer.cs
@@ -13,11 +13,20 @@ namespace ShootingEditor2D
             XmlDocument document = new XmlDocument();
             document.LoadXml(xml);
 
-            XmlNode levelNode = document.SelectSingleNode("Level");
+            XmlNode levelNode = document.SelectSingleNode("level");
 
             foreach (XmlElement levelItemNode in levelNode.ChildNodes)
             {
+                // 解析 XML 文件
+                string levelItemName = levelItemNode.Attributes["name"].Value;
+                float levelItemX = float.Parse(levelItemNode.Attributes["x"].Value);
+                float levelItemY = float.Parse(levelItemNode.Attributes["y"].Value);
+                Debug.Log(levelItemName + ": (" + levelItemY + ", " + levelItemX + ")");
 
+                // 加载相应的预制体
+                GameObject levelItemPrefab = Resources.Load<GameObject>(levelItemName);
+                GameObject levelItemGameObj = Instantiate(levelItemPrefab, transform);
+                levelItemGameObj.transform.position = new Vector3(levelItemX, levelItemY, 0);
             }
         }
     }
