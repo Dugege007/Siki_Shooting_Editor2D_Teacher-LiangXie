db6b99df2d9f327dfd358a578772bcd6dc6dd96e
Dugege007 <351080175@qq.com>
2023-08-26 21:20:32 +0800

    044-是否重复绘制检测

--- Assets/Scripts/ViewController/LevelEditor/LevelEditor.cs
+++ Assets/Scripts/ViewController/LevelEditor/LevelEditor.cs
@@ -6,6 +6,11 @@ namespace ShootingEditor2D
     {
         public SpriteRenderer EmptyHighLight;
 
+        private int mCurrentHighlightPosX;
+        private int mCurrentHighlightPosY;
+
+        private bool mCanDraw;
+
         private void Update()
         {
             Vector3 mousePos = Input.mousePosition;
@@ -15,14 +20,41 @@ namespace ShootingEditor2D
             mouseWorldPos.x = Mathf.Floor(mouseWorldPos.x + 0.5f);
             mouseWorldPos.y = Mathf.Floor(mouseWorldPos.y + 0.5f);
             mouseWorldPos.z = 0;
-            EmptyHighLight.transform.position = mouseWorldPos;
+
+            if (Mathf.Abs(mCurrentHighlightPosX - mouseWorldPos.x) < 0.1f &&
+                Mathf.Abs(mCurrentHighlightPosY - mouseWorldPos.y) < 0.1f)
+            {
+
+            }
+            else
+            {
+                Vector3 highLightPos = mouseWorldPos;
+                highLightPos.z = -9f;
+                EmptyHighLight.transform.position = highLightPos;
+
+                Ray ray = Camera.main.ScreenPointToRay(mousePos);
+                RaycastHit2D hit = Physics2D.Raycast(ray.origin, Vector2.zero, 20);
+                if (hit.collider)
+                {
+                    EmptyHighLight.color = new Color(1, 0, 0, 0.5f);
+                    mCanDraw = false;
+                }
+                else
+                {
+                    EmptyHighLight.color = new Color(1, 1, 1, 0.5f);
+                    mCanDraw = true;
+                }
+            }
 
             if (Input.GetMouseButtonDown(0))
             {
-                GameObject groundPrefab = Resources.Load<GameObject>("Level/Ground");
-                GameObject groundObj = Instantiate(groundPrefab, transform);
-                groundObj.transform.position = mouseWorldPos;
-                groundObj.name = "Ground";
+                if (mCanDraw)
+                {
+                    GameObject groundPrefab = Resources.Load<GameObject>("Level/Ground");
+                    GameObject groundObj = Instantiate(groundPrefab, transform);
+                    groundObj.transform.position = mouseWorldPos;
+                    groundObj.name = "Ground";
+                }
             }
         }
     }
