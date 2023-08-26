d2f68f110abf7bd019a89d40a3e7e78b0cde0dc4
Dugege007 <351080175@qq.com>
2023-08-26 20:36:36 +0800

    043-单击绘制地块

--- Assets/Scripts/ViewController/LevelEditor/LevelEditor.cs
+++ Assets/Scripts/ViewController/LevelEditor/LevelEditor.cs
@@ -11,8 +11,19 @@ namespace ShootingEditor2D
             Vector3 mousePos = Input.mousePosition;
             Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint(mousePos);
 
+            // 坐标取整（四舍五入）
+            mouseWorldPos.x = Mathf.Floor(mouseWorldPos.x + 0.5f);
+            mouseWorldPos.y = Mathf.Floor(mouseWorldPos.y + 0.5f);
             mouseWorldPos.z = 0;
             EmptyHighLight.transform.position = mouseWorldPos;
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                GameObject groundPrefab = Resources.Load<GameObject>("Level/Ground");
+                GameObject groundObj = Instantiate(groundPrefab, transform);
+                groundObj.transform.position = mouseWorldPos;
+                groundObj.name = "Ground";
+            }
         }
     }
 }
