3c3f780c4c4ecc09baaca799610fdc91bde807da
Dugege007 <351080175@qq.com>
2023-08-26 22:32:25 +0800

    046-零点无法绘制问题修复 与 连续绘制功能

--- Assets/Scripts/ViewController/LevelEditor/LevelEditor.cs
+++ Assets/Scripts/ViewController/LevelEditor/LevelEditor.cs
@@ -11,15 +11,11 @@ namespace ShootingEditor2D
             Erase
         }
 
-        public SpriteRenderer EmptyHighLight;
-
         private OperateMode mCurrentOperateMode;
 
-        private int mCurrentHighlightPosX;
-        private int mCurrentHighlightPosY;
-        private bool mCanDraw;
-
+        public SpriteRenderer EmptyHighLight;
         private GameObject mCurrentObjectMouseOn;
+        private bool mCanDraw;
 
         private Lazy<GUIStyle> mModelLabelStyle = new Lazy<GUIStyle>(() => new GUIStyle(GUI.skin.label)
         {
@@ -38,11 +34,11 @@ namespace ShootingEditor2D
             Rect modeLabelRect = RectHelper.RectForAnchorCenter(Screen.width * 0.5f, 35f, 300f, 50f);
             GUI.Label(modeLabelRect, mCurrentOperateMode.ToString(), mModelLabelStyle.Value);
 
-            var drawButtonRect = new Rect(20, 20, 200, 60);
+            Rect drawButtonRect = new Rect(20, 20, 200, 60);
             if (GUI.Button(drawButtonRect, "绘制", mButtonStyle.Value))
                 mCurrentOperateMode = OperateMode.Draw;
 
-            var eraseButtonRect = new Rect(20, 100, 200, 60);
+            Rect eraseButtonRect = new Rect(20, 100, 200, 60);
             if (GUI.Button(eraseButtonRect, "橡皮", mButtonStyle.Value))
                 mCurrentOperateMode = OperateMode.Erase;
         }
@@ -52,22 +48,17 @@ namespace ShootingEditor2D
             Vector3 mousePos = Input.mousePosition;
             Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint(mousePos);
 
-            // 坐标取整（四舍五入）
-            mouseWorldPos.x = Mathf.Floor(mouseWorldPos.x + 0.5f);
+            mouseWorldPos.x = Mathf.Floor(mouseWorldPos.x + 0.5f); // 坐标取整（四舍五入）
             mouseWorldPos.y = Mathf.Floor(mouseWorldPos.y + 0.5f);
             mouseWorldPos.z = 0;
 
             if (GUIUtility.hotControl == 0)
-            {
                 EmptyHighLight.gameObject.SetActive(true);
-            }
             else
-            {
                 EmptyHighLight.gameObject.SetActive(false);
-            }
 
-            if (Mathf.Abs(mCurrentHighlightPosX - mouseWorldPos.x) < 0.1f &&
-                Mathf.Abs(mCurrentHighlightPosY - mouseWorldPos.y) < 0.1f)
+            if (Mathf.Abs(EmptyHighLight.transform.position.x - mouseWorldPos.x) < 0.1f &&
+                Mathf.Abs(EmptyHighLight.transform.position.y - mouseWorldPos.y) < 0.1f)
             {
 
             }
@@ -82,34 +73,28 @@ namespace ShootingEditor2D
                 if (hit.collider)
                 {
                     if (mCurrentOperateMode == OperateMode.Draw)
-                    {
                         EmptyHighLight.color = new Color(1, 0, 0, 0.5f);    // 红色
-                    }
                     else if (mCurrentOperateMode == OperateMode.Erase)
-                    {
                         EmptyHighLight.color = new Color(1, 0.5f, 0, 0.5f); // 橙色
-                    }
-                    mCanDraw = false;
 
+                    mCanDraw = false;
                     mCurrentObjectMouseOn = hit.collider.gameObject;
                 }
                 else
                 {
                     if (mCurrentOperateMode == OperateMode.Draw)
-                    {
                         EmptyHighLight.color = new Color(1, 1, 1, 0.5f);    // 白色
-                    }
                     else if (mCurrentOperateMode == OperateMode.Erase)
-                    {
                         EmptyHighLight.color = new Color(0, 0, 1, 0.5f);    // 蓝色
-                    }
-                    mCanDraw = true;
 
+                    mCanDraw = true;
                     mCurrentObjectMouseOn = null;
                 }
             }
 
-            if (Input.GetMouseButtonDown(0) && GUIUtility.hotControl == 0)
+            if (Input.GetMouseButtonDown(0) ||
+                Input.GetMouseButton(0) &&
+                GUIUtility.hotControl == 0)
             {
                 if (mCanDraw && mCurrentOperateMode == OperateMode.Draw)
                 {
@@ -117,10 +102,14 @@ namespace ShootingEditor2D
                     GameObject groundObj = Instantiate(groundPrefab, transform);
                     groundObj.transform.position = mouseWorldPos;
                     groundObj.name = "Ground";
+
+                    mCanDraw = false;
                 }
                 else if (mCurrentObjectMouseOn && mCurrentOperateMode == OperateMode.Erase)
                 {
                     Destroy(mCurrentObjectMouseOn);
+
+                    mCurrentObjectMouseOn = null;
                 }
             }
         }
